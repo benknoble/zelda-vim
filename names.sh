5058027b2791a8b7913f3b58e139d55358e9033d
David Ben Knoble <ben.knoble@gmail.com>
2018-02-17 18:45:57 +0100

    Start the names.sh script
    This script will be useful as the number of colorschemes grow to
    maintain a list in the README.md

---
+++ names.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# A short program to generate the listing of colorscheme names for the README
+# based on the files in `colors`
+
+set -euo pipefail
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+
+main() {
+  echo "Not there yet"
+}
+
+main
